5ed7258fa7861a10952f6c5e4961bc29e5b6f78d
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-04 02:55:45 +0100

    lsat update

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
             <div class="w-full">
                 <h1 class="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
-                    S"incrire a notre formation 
+                    S`&apos;`incrire a notre formation 
                 </h1>
 
                 <p class="mt-4 text-gray-500 dark:text-gray-400">
@@ -72,7 +72,7 @@ Illustrator</option>
 
                     <button
                         class="flex items-center justify-content-center justify-between w-80 px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50 ">
-                        <span>S"incrire </span>
+                        <span>S`&apos;`incrire </span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd"
