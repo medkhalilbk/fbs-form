6be5cb0973c42bfbc38e3fafc8e35af3e10860f6
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-06 14:18:17 +0100

    deplying changing name

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath:'/inscription' , 
+  async redirects() {
+    return [
+        {
+            source: '/',
+            destination: '/inscription',
+            basePath: false,
+            permanent: false
+        }
+    ]
+}
 }
 
 module.exports = nextConfig
