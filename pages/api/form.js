519a72127f6bd0946cd6a5ec1656779db498827a
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-06 11:02:34 +0100

    updatez

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,21 +1,27 @@
-import axios from "axios"
-
-export default function handler(req, res) {
+import axios from "axios" 
+import { redirect } from 'next/navigation';
+export default async function handler(req, res) {
     // Get data submitted in request's body.
     const body = req.body
   
     // Optional logging to see the responses
     // in the command line where next.js app is running.
-    console.log('body: ', body)
+ 
   
     // Guard clause checks for first and last name,
     // and returns early if they are not found
   
       // Sends a HTTP bad request error code
-      axios.post('https://sheet.best/api/sheets/049e7c3e-18a0-4ff3-b540-494bd3d20598' , body).then((res) => {
-        console.log(res)
-      })
-      return res.redirect('/')
+     try {
+      let data = await axios.post('https://sheet.best/api/sheets/049e7c3e-18a0-4ff3-b540-494bd3d20598' , body)
+      console.log(data)
+      redirect('/')
+     } catch (error) {
+
+      console.log(error)
+      redirect('/')
+  
+     } 
  
   
     // Found the name.
