069c9ea5798a5ee46cf2f3f5f02564a669919820
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-08 01:17:55 +0100

    lastest

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1179117399462062')
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+  
+
   return <Component {...pageProps} />
 }
