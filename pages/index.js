44968e8339e4172ae7380b63b04641ffa91c3a8e
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-08 01:27:47 +0100

    latset

--- pages/index.js
+++ pages/index.js
@@ -186,7 +186,7 @@ export default function Home() {
  
 }
 <button  disabled={isSent} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-  S&#39;incrire
+  S&#39;inscrire
 </button>
      
 
